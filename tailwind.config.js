9fc66be36309d7e4b83d62a1f532abfb5f2a62c5
rxthew <richardtheumaster@gmail.com>
2022-06-14 23:17:46 +0200

    overlay banner (incl. github link) & font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'norse' : ['norse', ...defaultTheme.fontFamily.mono]    
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
